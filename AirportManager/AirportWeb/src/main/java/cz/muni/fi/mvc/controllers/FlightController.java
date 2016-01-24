bb7b7be494194e0133c622980e202d5ac2ccc6d1
bossbast1 <bast1@centrum.sk>
2016-01-24 22:35:20 +0100

    revert mojich uprav

--- AirportManager/AirportWeb/src/main/java/cz/muni/fi/mvc/controllers/FlightController.java
+++ AirportManager/AirportWeb/src/main/java/cz/muni/fi/mvc/controllers/FlightController.java
@@ -49,44 +49,44 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 /**
  * SpringMVC Controller for Flight management
- *
  * @author Gabriela Podolnikova
  */
 @Import({FacadeTestConfiguration.class})
 @RequestMapping("/flight")
 @Controller
 public class FlightController {
-
+    
     final static Logger log = LoggerFactory.getLogger(FlightController.class);
 
     @Autowired
     private FlightFacade flightFacade;
-
+    
     @Autowired
     private StewardFacade stewardFacade;
-
+    
     @Autowired
     private DestinationFacade destinationFacade;
-
+    
     @Autowired
     private AirplaneFacade airplaneFacade;
-
+    
+    
+    
     @InitBinder
     protected void initBinder(WebDataBinder binder) {
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
         binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, false));
         /*if (binder.getTarget() instanceof FlightCreationalDTO) {
-         binder.addValidators(new FlightCreationalDTOValidator(flightFacade));
-         }*/
-
+            binder.addValidators(new FlightCreationalDTOValidator(flightFacade));
+        }*/
+        
         if (binder.getTarget() instanceof UpdateFlightDTO) {
             binder.addValidators(new FlightUpdateDTOValidator(flightFacade));
         }
     }
-
-    /**
+    
+     /**
      * Creates form for Flight creation
-     *
      * @param model display data
      * @return jsp page
      */
@@ -99,16 +99,15 @@ public class FlightController {
         model.addAttribute("airplanes", airplaneFacade.getAllAirplanes());
         return "flight/new";
     }
-
-    /**
+    
+     /**
      * Creates a new Flight
-     *
      * @param model display data
      * @return jsp page
      */
     @RequestMapping(method = RequestMethod.POST, value = "/create")
     public String create(@Valid @ModelAttribute("flightCreate") FlightCreationalDTO formBean, BindingResult bindingResult,
-            Model model, RedirectAttributes redirectAttributes, UriComponentsBuilder uriBuilder) {
+                         Model model, RedirectAttributes redirectAttributes, UriComponentsBuilder uriBuilder) {
         if (bindingResult.hasErrors()) {
             for (FieldError fe : bindingResult.getFieldErrors()) {
                 model.addAttribute(fe.getField() + "_error", true);
@@ -117,41 +116,34 @@ public class FlightController {
             for (ObjectError ge : bindingResult.getGlobalErrors()) {
                 log.trace("ObjectError: {}", ge);
             }
-            model.addAttribute("destinations", destinationFacade.getAllDestinations());
-            model.addAttribute("stewards", stewardFacade.getAllStewards());
-            model.addAttribute("airplanes", airplaneFacade.getAllAirplanes());
-
             return "flight/new";
         }
         Long id = flightFacade.createFlight(formBean);
-
         redirectAttributes.addFlashAttribute("alert_info", "Flight with id: " + id + " was created");
         return "redirect:" + uriBuilder.path("/flight").toUriString();
     }
-
+    
     /**
      * Deletes a Flight
-     *
      * @param id flight's id
      * @param model display data
      * @return jsp page
      */
     @RequestMapping(value = "/delete/{id}", method = {RequestMethod.POST, RequestMethod.GET})
-    public String delete(@PathVariable long id, Model model, HttpServletRequest request,
+    public String delete(@PathVariable long id, Model model, HttpServletRequest  request,
             UriComponentsBuilder uriBuilder, RedirectAttributes redirectAttributes) {
         FlightDTO flight = flightFacade.getFlightWithId(id);
         flightFacade.removeFlight(id);
         redirectAttributes.addFlashAttribute("alert_info", "Flight with id: " + flight.getId() + " was deleted.");
-        UriComponentsBuilder build = uriBuilder.path("/flight");
-        for (Entry<String, String[]> pair : request.getParameterMap().entrySet()) {
+        UriComponentsBuilder build =  uriBuilder.path("/flight");
+        for ( Entry<String,String[]> pair : request.getParameterMap().entrySet()) {
             build.queryParam(pair.getKey(), pair.getValue());
-        }
+        }     
         return "redirect:" + build.toUriString();
     }
-
+    
     /**
      * Shows a Flight
-     *
      * @param id identificator of flight
      * @param model display data
      * @return jsp page
@@ -160,20 +152,21 @@ public class FlightController {
     public String detail(@PathVariable long id, Model model, RedirectAttributes redirectAttributes, UriComponentsBuilder uriBuilder) {
         try {
             model.addAttribute("flight", flightFacade.getFlightWithId(id));
-        } catch (Exception e) {
+        }
+        catch (Exception e) {
             redirectAttributes.addFlashAttribute("alert_danger", "Flight with id: " + id + " was not found.");
             return "redirect:" + uriBuilder.path("/flight").toUriString();
         }
         return "flight/detail";
     }
-
+     
     /**
      * Prepares edit form.
      *
-     * @param id, model
+     * @param id, model 
      * @return JSP page to edit flight
      */
-    @RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+    @RequestMapping(value = "/edit/{id}",method = RequestMethod.GET)
     public String editFlight(@PathVariable("id") long id, Model model, RedirectAttributes redirectAttributes, UriComponentsBuilder uriBuilder) {
         UpdateFlightDTO flight = flightFacade.getUpdateFlightWithId(id);
         if (flight == null) {
@@ -186,17 +179,16 @@ public class FlightController {
         model.addAttribute("airplanes", airplaneFacade.getAllAirplanes());
         return "flight/edit";
     }
-
-    /**
+    
+     /**
      * Updates flight
      *
-     * @param id, modelAttribute, bindingResult, model, redirectAttributes,
-     * uriBuilder
+     * @param id, modelAttribute, bindingResult, model, redirectAttributes, uriBuilder
      * @return JSP page
      */
     @RequestMapping(value = "/update/{id}", method = RequestMethod.POST)
     public String updateFlight(@PathVariable("id") long id, @Valid @ModelAttribute("flight") UpdateFlightDTO updatedFlight, BindingResult bindingResult,
-            Model model, RedirectAttributes redirectAttributes, UriComponentsBuilder uriBuilder) {
+                         Model model, RedirectAttributes redirectAttributes, UriComponentsBuilder uriBuilder){
 
         try {
             FlightDTO flight = flightFacade.getFlightWithId(id);
@@ -213,15 +205,15 @@ public class FlightController {
         redirectAttributes.addFlashAttribute("alert_success", "Flight " + id + " was updated");
         return "redirect:" + uriBuilder.path("/flight").toUriString();
     }
-
+    
     @ModelAttribute("locations")
     public Location[] locations() {
         log.debug("locations()");
         return Location.values();
     }
-
+    
     /**
-     * Shows a list of flights which are available at given location
+     * Shows a list of flights which are available at given location 
      *
      * @param locationType specifies, if it is for Origin, or Destination
      * @param locationId id of location, for which we want to find flights
@@ -251,20 +243,23 @@ public class FlightController {
 
         if (!invalid) {
             try {
-
+                
                 DestinationDTO destination = null;
-
+                
                 if (locationId != null) {
                     destination = destinationFacade.getDestinationWithId(locationId);
                 }
 
                 List<FlightDTO> flights = new ArrayList<>();
-
+                
+                
                 if (destination == null || locationType == Location.NONE) {
                     flights = flightFacade.getAllFlights();
-                } else if (locationType == Location.DESTINATION) {
+                } 
+                else if (locationType == Location.DESTINATION) {
                     flights = flightFacade.getFlightsByDestination(destination);
-                } else if (locationType == Location.ORIGIN) {
+                }
+                else if (locationType == Location.ORIGIN) {
                     flights = flightFacade.getFlightsByOrigin(destination);
                 }
                 model.addAttribute("flights", flights);
